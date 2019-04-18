0f7342bf6c9ba1ad5273ec88d4f4fbc1442b6c42
Maneesh Verma <maneeshv11@gmail.com>
2019-04-18 23:55:21 +0530

    values in go(#2)

---
+++ values/values.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	//comment
+
+	// string
+	fmt.Println("My name is " + "Maneesh")
+
+	fmt.Println("1+1 is ", 1+1)
+
+	fmt.Println("1+1.0 is ", 1+1.0)
+
+	fmt.Println("7/3 is ", 7/3)
+
+	fmt.Println("7/3.0 is ", 7/3.0)
+
+
+	// boolean
+
+	fmt.Println("true||false is ", true || false)
+	fmt.Println("true && false is ", true && false)
+	fmt.Println("!true is ", !true)
+
+}
